542c63d870d338b8b6a0ca0f2692b998690def31
Bokyung <am322@naver.com>
2020-12-13 21:30:23 +0900

    책장컬렉션 오타 수정했습니다.. 죄송합니당

--- src/rw/col/bookshelf/model/vo/BookshelfCollection.java
+++ src/rw/col/bookshelf/model/vo/BookshelfCollection.java
@@ -3,7 +3,7 @@ package rw.col.bookshelf.model.vo;
 public class BookshelfCollection {
 	private int colBookshelfId;
 	private String memberNo;
-	private String bookselfId;
+	private String bookshelfId;
 	public int getColBookshelfId() {
 		return colBookshelfId;
 	}
@@ -16,22 +16,25 @@ public class BookshelfCollection {
 	public void setMemberNo(String memberNo) {
 		this.memberNo = memberNo;
 	}
-	public String getBookselfId() {
-		return bookselfId;
+	
+	
+	public String getBookshelfId() {
+		return bookshelfId;
 	}
-	public void setBookselfId(String bookselfId) {
-		this.bookselfId = bookselfId;
+	public void setBookshelfId(String bookshelfId) {
+		this.bookshelfId = bookshelfId;
 	}
 	public BookshelfCollection() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public BookshelfCollection(int colBookshelfId, String memberNo, String bookselfId) {
+	public BookshelfCollection(int colBookshelfId, String memberNo, String bookshelfId) {
 		super();
 		this.colBookshelfId = colBookshelfId;
 		this.memberNo = memberNo;
-		this.bookselfId = bookselfId;
+		this.bookshelfId = bookshelfId;
 	}
+	
 	
 	
 }
