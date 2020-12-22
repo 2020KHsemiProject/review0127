2ed82567215059eaafb403aecd06a6957298d1ff
jayesay <jayesay52@gmail.com>
2020-12-22 15:10:27 +0900

    머지 해결

--- src/rw/common/MailAuthentication.java
+++ src/rw/common/MailAuthentication.java
@@ -10,8 +10,6 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-import rw.member.model.vo.Member;
-
 
 public class MailAuthentication {
 	
